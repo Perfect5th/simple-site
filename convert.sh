4fbb31722574debcbb8139b3e994daa2e4c0a314
Mitchell Burton <mitch@mitchellburton.ca>
2024-01-21 11:43:58 -0800

    env-informed site root

--- convert.sh
+++ convert.sh
@@ -11,7 +11,7 @@ mkdir -p ./_site
 for FILE in $FILES; do
     HTML_PATH=$(echo "$FILE" | sed -e 's/\.md$/\.html/' | sed -e 's/^\.\/markdown/\.\/_site/')
     echo "Creating $HTML_PATH"
-    cat ./templates/base.html > "$HTML_PATH"
+    sed -e "s:{{SITE_ROOT}}:$SITE_ROOT:g" ./templates/base.html > "$HTML_PATH"
     markdown --html4tags "$FILE" >> "$HTML_PATH"
     cat ./templates/close.html >> "$HTML_PATH"
 done
